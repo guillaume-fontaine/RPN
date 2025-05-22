45210aa8867d28ed5e23bc737e54b59f40d306f9
trollgun <guillaume.fontaine.ab@gmail.com>
2025-05-22 11:57:57 +0200

    Implement expression evaluation in RpnApp using a stack and regex for tokenization

--- src/main/java/org/example/RpnApp.java
+++ src/main/java/org/example/RpnApp.java
@@ -9,6 +9,11 @@ public class RpnApp {
     }
 
     public static int evaluate(String expression) {
+        //Une expression est séparé par un ou plusieurs espaces
+        //On utilise une pile pour stocké les chiffres
+        //On divise l'expression avec une regex sur les espaces
+        Stack<Integer> stack = new Stack<>();
+        String[] tokens = expression.split(" ");
         return 0;
     }
 }
